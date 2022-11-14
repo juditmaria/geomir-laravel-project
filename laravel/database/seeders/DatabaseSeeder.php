57087d51d708c23c77166dbdc2ac7f16089170a8
judit maria <jumoar@fp.insjoaquimmir.cat>
2022-11-14 19:08:41 +0100

    [M7] 3.3 BD

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -1,6 +1,26 @@
 <?php
 
 namespace Database\Seeders;
+ 
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+ 
+class UserSeeder extends Seeder
+{
+   public function run()
+   {
+       $admin = new User([
+           'name'      => config('admin.name'),
+           'email'     => config('admin.email'),
+           'password'  => Hash::make(config('admin.password')),
+       ]);
+       $admin->save();
+   }
+}
+
+/*
+namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,7 +32,7 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+  /*  public function run()
     {
         // \App\Models\User::factory(10)->create();
 
@@ -22,3 +42,4 @@ class DatabaseSeeder extends Seeder
         // ]);
     }
 }
+*/
